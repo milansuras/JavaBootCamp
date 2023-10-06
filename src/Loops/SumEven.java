f6e69353879c111012f4e6d70872f7a945b1d84b
MILAN_SURAS <milanmidhun2@gmail.com>
2023-10-06 15:14:03 +0530

    Sum of even number with in a range

--- src/Loops/SumEven.java
+++ src/Loops/SumEven.java
@@ -1,2 +1,14 @@
-package Loops;public class SumEven {
+package Loops;
+import java.util.Scanner;
+public class SumEven {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the range to check the sum:");
+         int n=sc.nextInt();
+         int sum=0;
+         for(int i=0; i<=n; i+=2){
+             sum+=i;
+         }
+         System.out.println("The sum of even num  in range of " + 0 + " to " + n + " = " +sum);
+    }
 }
