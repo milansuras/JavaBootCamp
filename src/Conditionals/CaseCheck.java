29c5a4777bc3e076211a3dead553daa975e65c0d
MILAN_SURAS <milanmidhun2@gmail.com>
2023-10-06 13:51:09 +0530

    Alphabet case check

--- src/Conditionals/CaseCheck.java
+++ src/Conditionals/CaseCheck.java
@@ -1,2 +1,15 @@
-package Conditionals;public class CaseCheck {
+package Conditionals;
+import java.util.Scanner;
+public class CaseCheck {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any charachter:-");
+        char ch=sc.next().trim().charAt(0);
+        if(ch >='a' && ch <='z'){
+            System.out.println("Entered charachter is lowercase");
+        }
+        else
+            System.out.println("Entered charachter is uppercase");
+
+    }
 }
