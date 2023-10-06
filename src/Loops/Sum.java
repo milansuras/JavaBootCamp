bf72b95e10327cbc38b5bcf5dc6dec0402f45e3a
MILAN_SURAS <milanmidhun2@gmail.com>
2023-10-06 16:01:23 +0530

    Sum of n numbers

---
+++ src/Loops/Sum.java
@@ -0,0 +1,14 @@
+package Loops;
+import java.util.Scanner;
+public class Sum {
+    public static void main(String[]  args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:-");
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=1; i<=n; i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
