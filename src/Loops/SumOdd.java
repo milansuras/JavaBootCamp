ad7aef27b07391a73425d1cb536630e5491b39d3
MILAN_SURAS <milanmidhun2@gmail.com>
2023-10-06 15:15:07 +0530

    Sum of odd number in a range

--- src/Loops/SumOdd.java
+++ src/Loops/SumOdd.java
@@ -1,2 +1,15 @@
-package Loops;public class SumOdd {
+package Loops;
+import java.util.Scanner;
+public class SumOdd {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the range:-");
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=1; i<=n; i+=2){
+            sum+=i;
+        }
+        System.out.println("Sum of odd num in range of " + 1 + " to " + " n " + " =" +sum);
+
+    }
 }
