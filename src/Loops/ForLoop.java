2ef86c2e0a5a0bedd0c158e870d8b1c63b206bdf
MILAN_SURAS <milanmidhun2@gmail.com>
2023-10-06 15:13:27 +0530

    Loops

--- src/Loops/ForLoop.java
+++ src/Loops/ForLoop.java
@@ -1,2 +1,9 @@
-package Loops;public class ForLoop {
+package Loops;
+
+public class ForLoop {
+    public static void main(String[ ] args){
+        for(int i=1; i<=7; i++){
+            System.out.println("Hello World!");
+        }
+    }
 }
--- src/Loops/WhileLoop.java
+++ src/Loops/WhileLoop.java
@@ -1,2 +1,11 @@
-package Loops;public class WhileLoop {
+package Loops;
+
+public class WhileLoop {
+    public static void main(String[] args){
+        int i=1; //Initialization
+        while(i<=7){ // condition
+            System.out.println("I love Java");
+            i++; // updation
+        }
+    }
 }
