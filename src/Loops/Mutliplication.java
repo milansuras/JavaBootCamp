8e592581fc804c27b7f75a37e1a0047835c12f67
MILAN_SURAS <milanmidhun2@gmail.com>
2023-10-07 19:31:25 +0530

    Greeting

--- src/Loops/Mutliplication.java
+++ src/Loops/Mutliplication.java
@@ -1,2 +1,16 @@
-package Loops;public class Mutliplication {
+package Loops;
+import java.util.Scanner;
+public class Mutliplication {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int num,n;
+        System.out.println("Enter num to get multiplication table:-");
+         num=sc.nextInt();
+        System.out.println("Enter the range :-");
+        n=sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            System.out.println(i + " * " + n + " = " + i*num);
+        }
+    }
 }
---
+++ src/Methods/Greet.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Greet {
+    public static void main(String[] args){
+        greeting();
+    }
+
+    static void greeting(){
+        System.out.println("Hello ");
+    }
+}
