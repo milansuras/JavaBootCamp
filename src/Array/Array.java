7bd724b543d1fa1a375ac5bef1ba988ec8554441
MILAN_SURAS <milanmidhun2@gmail.com>
2023-10-10 19:31:17 +0530

    Introduction to array

---
+++ src/Array/Array.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Array {
+    public static void main(String[] args){
+        // syntax of array
+        int[] arr={1,2,3,4,5,6,7,8};
+
+        for(int i=0; i<arr.length-1; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
