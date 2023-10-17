55f6a873dd3659dfc85456dd7f4efcef8263de5a
MILAN_SURAS <milanmidhun2@gmail.com>
2023-10-17 15:17:47 +0530

    Number pyramid pattern

---
+++ src/Patterns/NumberPyramid.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+
+
+public class NumberPyramid {
+    public static void main(String[] args) {
+        System.out.println("Number increasing Pyramid");
+        for(int i=1; i<=7; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
